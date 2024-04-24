a3b8e4aeb395cf6517943647ede36c15d8808b35
HKEV <devsaleh.ismail01@gmail.com>
2024-04-24 20:04:46 +0100

    last

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -5,6 +5,7 @@ sleep 10
 cd /var/www/wordpress
 
 wp core download --allow-root
+
 wp config create --dbhost=$DB_HOST \
                     --dbname=$DB_NAME \
                     --dbuser=$DB_USER \
@@ -20,9 +21,6 @@ wp core install --title=$WP_TITLE \
 
 wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD  --allow-root
 
-
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-# cd -
 
 /usr/sbin/php-fpm7.4 -F
